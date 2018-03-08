aef83e2bf7d8a37cf24c79a66a742c9b5592e344
ts-takao.a.fukuoka <ts-takao.a.fukuoka@rakuten.com>
2018-03-08 11:15:36 +0900

    add coholately

---
+++ .chocolately/install.bat
@@ -0,0 +1,3 @@
+# Installing Chocolatey
+# see. https://github.com/chocolatey/choco/wiki/Installation
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
